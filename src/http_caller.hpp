5c2dadbbc7214a846d44b2a37bd8f107418727b3
liut <liut@phoenixtv.com>
2021-09-09 14:45:56 +0800

    structure http callers

--- src/http_caller.hpp
+++ src/http_caller.hpp
@@ -44,29 +44,44 @@ public:
     const std::optional<http::response<string_body>>
     call_sb(http::request<RequestBody> const& req)
     {
-	    if (req.method() == verb::get && req.target() == "/sess"sv)
-	    {
-	    	auto const settings = shth_->sess()->get_settings();
-	    	auto const peerID = settings.get_str(lt::settings_pack::peer_fingerprint);
-	    	auto const peerPort = parse_port(settings.get_str(lt::settings_pack::listen_interfaces));
-	    	const json::value jv= {{"peerID", peerID}, {"peerPort", peerPort}
-	            , {"uptime", uptime()}, {"uptimeMs", uptimeMs()}, {"version", lt::version()}};
-	        return make_resp<string_body>(req, json::serialize(jv), ctJSON);
-	    }
+    	if (req.method() == verb::get && req.target() == "/sess"sv) return getSession(req);
+    	if (req.method() == verb::get && req.target() == "/stats"sv) return getStats(req);
+    	if (req.target() == "/torrents"sv) return handleTorrents(req);
 
-	    if (req.method() == verb::get && req.target() == "/stats"sv)
-	    {
-	    	return make_resp<string_body>(req, json::serialize(shth_->get_stats_json()), ctJSON);
-	    }
+	    if (req.target().find("/torrent/") == 0) return handleTorrent(req);
 
-	    if (req.method() == verb::get && req.target() == "/torrents"sv)
-	    {
-	    	return make_resp<string_body>(req, json::serialize(shth_->get_torrents()), ctJSON);
-	    }
+    	// TODO: more routes
+    	return std::nullopt;
+    }
 
-	    if (req.method() == verb::post && req.target() == "/torrents"sv)
-		{
-			std::clog << " post clen " << req.find(http::field::content_length)->value() << " bytes\n";
+    http::response<string_body>
+    getSession(http::request<string_body> const& req)
+    {
+    	auto const settings = shth_->sess()->get_settings();
+    	auto const peerID = settings.get_str(lt::settings_pack::peer_fingerprint);
+    	auto const peerPort = parse_port(settings.get_str(lt::settings_pack::listen_interfaces));
+    	const json::value jv= {{"peerID", peerID}, {"peerPort", peerPort}
+            , {"uptime", uptime()}, {"uptimeMs", uptimeMs()}, {"version", lt::version()}};
+        return make_resp<string_body>(req, json::serialize(jv), ctJSON);
+    }
+
+    http::response<string_body>
+    getStats(http::request<string_body> const& req)
+    {
+    	return make_resp<string_body>(req, json::serialize(shth_->get_stats_json()), ctJSON);
+    }
+
+    http::response<string_body>
+    handleTorrents(http::request<string_body> const& req) // get or post
+    {
+    	if (req.method() == verb::get)
+    	{
+    		return make_resp<string_body>(req, json::serialize(shth_->get_torrents()), ctJSON);
+    	}
+
+    	if (req.method() == verb::post)
+    	{
+    		std::clog << " post clen " << req.find(http::field::content_length)->value() << " bytes\n";
 			auto savePath = req.find("x-save-path");
 			if (savePath == req.end())
 			{
@@ -81,55 +96,55 @@ public:
 				return make_resp_204(req);
 			}
 			return make_resp_500(req, "failed to add");
-	    }
+    	}
 
-	    if (req.target().find("/torrent/") == 0)
-	    {
-	    	// /torrent/{info_hash}/{act}?
-			const std::string s(req.target().data(), req.target().size());
-			const int ps = 40 + 9;
-			if (s.size() < ps) return make_resp_404(req);
-
-			lt::sha1_hash ih;
-			if (!from_hex(ih, s.substr(9, 40)))
-			{
-				return make_resp_400(req, "invalid hash string");
-			}
-			if (req.method() == verb::head)
-			{
-				if (shth_->exists(ih))
-				{
-					return make_resp_204(req);
-				}
-				return make_resp_404(req);
-			}
-			std::string act = "";
-			if (s.size() > ps + 1)
-			{
-				act = s.substr(ps + 1);
-			}
-			if (req.method() == verb::get)
-			{
-				auto flag = sheath::query_basic;
-				if ("peers" == act) flag = sheath::query_peers;
-				else if ("files" == act) flag = sheath::query_files;
-				auto jv = shth_->get_torrent(ih, flag);
-				if (jv.is_null()) { return make_resp_404(req); }
-				return make_resp<string_body>(req, json::serialize(jv), ctJSON);
-			}
-			if (req.method() == verb::delete_)
-			{
-				shth_->drop_torrent(ih, ("yes" == act || "with_data" == act));
-				return make_resp_204(req);
-			}
-
-			return make_resp_400(req, "Unsupported method");
-	    }
-
-    	// TODO: more routes
-    	return std::nullopt;
+		return make_resp_400(req, "Unsupported method");
     }
 
+
+    http::response<string_body>
+    handleTorrent(http::request<string_body> const& req) // get or post or delete torrent
+	{
+    	// /torrent/{info_hash}/{act}?
+		const std::string s(req.target().data(), req.target().size());
+		const int ps = 40 + 9;
+		if (s.size() < ps) return make_resp_404(req);
+
+		lt::sha1_hash ih;
+		if (!from_hex(ih, s.substr(9, 40)))
+		{
+			return make_resp_400(req, "invalid hash string");
+		}
+		if (req.method() == verb::head)
+		{
+			if (shth_->exists(ih))
+			{
+				return make_resp_204(req);
+			}
+			return make_resp_404(req);
+		}
+		std::string act = "";
+		if (s.size() > ps + 1)
+		{
+			act = s.substr(ps + 1);
+		}
+		if (req.method() == verb::get)
+		{
+			auto flag = sheath::query_basic;
+			if ("peers" == act) flag = sheath::query_peers;
+			else if ("files" == act) flag = sheath::query_files;
+			auto jv = shth_->get_torrent(ih, flag);
+			if (jv.is_null()) { return make_resp_404(req); }
+			return make_resp<string_body>(req, json::serialize(jv), ctJSON);
+		}
+		if (req.method() == verb::delete_)
+		{
+			shth_->drop_torrent(ih, ("yes" == act || "with_data" == act));
+			return make_resp_204(req);
+		}
+
+		return make_resp_400(req, "Unsupported method");
+    }
 
 	void
 	join(websocket_session* session)
